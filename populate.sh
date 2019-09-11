15021009de731cc97cf41f924e3935f2c55d983b
Mukesh Joshi <mukesh.bciit@gmail.com>
2019-09-12 00:44:45 +0200

    Initial commit

---
+++ populate.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DB_USER='username'
+DB_PWD='password'
+
+ADMIN_USERS=
+
+# To fetch all users whose content is not updated.
+function getAdminUsers()
+{
+ADMIN_USERS=`
+SQLPLUS="$ORACLE_HOME/bin/sqlplus -s $DB_USER/$DB_PWD"
+$SQLPLUS << EOF_SQL_1
+        set feedback off
+        set heading off;
+        set serverout off;
+        set underline off
+        set verify off;
+        SELECT USER_NAME FROM USER WHERE TYPE='ADMIN' AND USER_ID NOT IN (SELECT USER_ID FROM BLOCKED_USERS);
+        exit;
+EOF_SQL_1`
+}
+
+# Insert and Update user content
+function updateUserAndContent()
+{
+SQLPLUS="$ORACLE_HOME/bin/sqlplus -s $DB_USER/$DB_PWD"
+$SQLPLUS << EOF_SQL_1
+        set feedback off
+        set heading off;
+        set serverout off;
+        set underline off
+        set verify off;
+		INSERT INTO USER_CONTENT VALUES (USER_SEQUENCE.nextval, null);
+		INSERT INTO USER_CONTENT VALUES (USER_SEQUENCE.currval, $1);
+		commit;
+        exit;
+EOF_SQL_1
+}
+
+# Create content for each empty user
+function createContentForUser
+{
+for userID in ${ADMIN_USERS}; do
+
+	printf "Content populated for user Id : [userID] \n"
+
+	#Add entry in user content tables
+	updateUserAndContent ${userID}
+
+done
+}
+
+#Get list of Admin users
+getAdminUsers
+
+#Populate content tables
+createContentForUser
